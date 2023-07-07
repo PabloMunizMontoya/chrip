fc38c44f2f070abeb1a7d2ea0f69b8e64cebe506
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-07-07 01:45:35 -0500

    router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { postsRouter } from "~/server/api/routers/posts";
-import { exampleRouter } from "~/server/api/routers/example";
+
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -8,7 +8,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  
   posts: postsRouter
 });
 
